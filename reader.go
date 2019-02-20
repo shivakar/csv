7107590c8a7110e52acdaf9e042d8260191221f3
Shivakar Vulli <svulli@cogolabs.com>
2019-02-20 00:04:53 -0500

    Cleanup reader implementation

--- reader.go
+++ reader.go
@@ -7,23 +7,12 @@ import (
 
 // Reader is a drop-in replacement for encoding/csv.Reader
 type Reader struct {
-	reader *lcsv.Reader
+	*lcsv.Reader
 }
 
 // NewReader returns a new CSV reader
 func NewReader(r io.Reader) *Reader {
-	rdr := &Reader{}
-	lr := lcsv.NewReader(r)
-	rdr.reader = lr
-	return rdr
-}
-
-// Read reads the next record from underlying reader
-func (r *Reader) Read() ([]string, error) {
-	return r.reader.Read()
-}
-
-// ReadAll reads all remaining records from underlying reader
-func (r *Reader) ReadAll() ([][]string, error) {
-	return r.reader.ReadAll()
+	return &Reader{
+		lcsv.NewReader(r),
+	}
 }
